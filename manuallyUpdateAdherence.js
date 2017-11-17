939f1adc5e0625a6ab353a777adf35c6c8dcf84e
devin <devincorrigall@gmail.com>
2017-11-16 17:06:02 -0800

    Fix delay

--- manuallyUpdateAdherence.js
+++ manuallyUpdateAdherence.js
@@ -15,11 +15,13 @@ var endDate = new Date(endString);
 
 console.log("Manually updating adherence hourly between " + startDate + " and " + endDate);
 
+var delayFactor = 1;
 while (startDate < endDate) {
 	let oldStartDate = new Date(startDate.getTime());
 	startDate.setHours(startDate.getHours() + 1);
 	// Add delay to avoid connection limit on heroku
-	setTimeout(function(from, to) {scheduledJobs.updateAdherence(from, to)}, 1000, oldStartDate, startDate);
+	setTimeout(function(from, to) {scheduledJobs.updateAdherence(from, to)}, 1000 * delayFactor, oldStartDate, startDate);
+	delayFactor = delayFactor + 1;
 }
 
 console.log("Manual update of adherence done.");
